0932ae30d6e1ed2f78a5b57e9f1b2bf49cac893a
Nicolas Gonzalez <ngonzalez@dminc.com>
2022-05-15 15:24:20 -0300

    Navbar added

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,32 @@
+import logo from '../../assets/images/Logo_ML.png';
+import search from '../../assets/images/ic_Search.png';
+import './navbar.scss';
+
+const Navbar = () => {
+  const handleClick = () => {
+    console.log('Funcion buscar');
+  };
+
+  return (
+    <nav className='navbar navbar-expand-lg navbar-light bg-yellow row justify-content-center gx-0'>
+      <div className="d-flex col-10">
+        <a className="navbar-brand me-4" href="/">
+          <img src={logo} alt="logo-mercado-libre" />
+        </a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <form className="w-100">
+            <div className="input-group">
+              <input type="text" className="form-control border-0" placeholder="Nunca dejes de buscar" aria-label="unca dejes de buscar" aria-describedby="basic-addon2" />
+              <span className="input-group-text border-0" id="basic-addon2" onClick={handleClick}><img src={search} alt="seach-icon" /></span>
+            </div>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
