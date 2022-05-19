ea7e000266bdb73d1e495f48967179c73dfd5bba
Nicolas Gonzalez <ngonzalez@dminc.com>
2022-05-19 12:08:56 -0300

    feat(detail page): detail page added

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -2,13 +2,14 @@ import PropTypes from 'prop-types';
 
 const Container = ({ children }) => {
   return (
-    <section className="bg-white rounded">
+    <section className={'bg-white rounded mb-5'}>
       {children}
     </section>
   );
 };
 
 Container.Proptypes = {
+  className: PropTypes.string,
   children: PropTypes.oneOfType([
     PropTypes.array,
     PropTypes.object,
