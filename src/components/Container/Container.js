2dfc4a720f905a6606ebb1f8645388c606848600
Nicolas Gonzalez <ngonzalez@dminc.com>
2022-05-19 12:11:39 -0300

    newline in scss and fix classname container component

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -2,14 +2,13 @@ import PropTypes from 'prop-types';
 
 const Container = ({ children }) => {
   return (
-    <section className={'bg-white rounded mb-5'}>
+    <section className="bg-white rounded mb-5">
       {children}
     </section>
   );
 };
 
 Container.Proptypes = {
-  className: PropTypes.string,
   children: PropTypes.oneOfType([
     PropTypes.array,
     PropTypes.object,
